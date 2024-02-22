a74ba8aaca258e6794e75b1809280f204d13f208
Hassan Ali <mhassan.ali7879@gmail.com>
2024-02-22 19:56:04 +0500

    Models are added and data is hardcoded to store.

--- ELibrary/Ebooks/views.py
+++ ELibrary/Ebooks/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 
 def index(request):
     return HttpResponse("Hello, world. You're in virtual world of learning skills.")
+
+
 def about(request):
+    b = Book(title="OOP", book_type="Computer Science", author="Robert Lafore", pub_date="1999-10-10")
+    b.save()
     return HttpResponse("It is online library where you can find any type of book you want")
+
